968e2886738d327e0edddfd73dc438f70267aa22
Narendhra kumar challa <naren@gmail.com>
2025-07-27 12:43:46 +0530

    AuthContext updated

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { Children, createContext, useEffect, useState } from 'react'
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:4000/api';
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
 console.log("base",baseUrl);
 
 axios.defaults.baseURL=baseUrl;
